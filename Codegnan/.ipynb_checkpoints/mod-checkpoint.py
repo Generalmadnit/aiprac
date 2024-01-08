3c988e455bfefc553704cec9794ac69e0b605d3f
generalmadnit <pathurinitin@gmail.com>
2024-01-08 08:26:05 +0530

    day changes

---
+++ Codegnan/.ipynb_checkpoints/mod-checkpoint.py
@@ -0,0 +1,8 @@
+'''
+import sample
+#help(sample)
+#print(dir(sample))
+print(type(sample.add))
+'''
+from sample import add
+print(add('Nitin',' Oracle'))
---
+++ Codegnan/.ipynb_checkpoints/sample-checkpoint.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    """
+    takes two variables a,b and returns the sum of them
+    """
+    return a+b
